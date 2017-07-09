46209bc96b8428c0dde80f75b22f2a8e268f6bd2
Idam Adam <idam.adam@gmail.com>
2017-07-09 11:32:44 +0000

    Add function to convert duration to minutes

--- walks/spiders/walks_spider.py
+++ walks/spiders/walks_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import json
 
 class WalkSpider(scrapy.Spider):
 	name = 'walks'
@@ -18,26 +19,32 @@ class WalkSpider(scrapy.Spider):
 			
 		def extract_with_css(query):
 			return response.css(query).extract_first().strip()
-
-		def is_walk_return(query):
-			returnVar = response.css(query).extract_first()
-			if returnVar != None:
-				return 'y'
-			else:
-				return 'n'
 		
-		def duration_in_min(number, unit):
-			durationTime = response.css(number)[1].extract()
-			durationUnit = response.css(unit)[1].extract()
+		def extract_stat_with_xpath(arrayIndex):
+			selector = '//div[@class="homeLists"]/div/div[@class="stat"][%s]/span/text()' % arrayIndex
+			return response.xpath(selector).extract()
+
+		def duration_in_min(arrayIndex):
+			data = extract_stat_with_xpath(arrayIndex)
 			
-			if durationUnit == 'days':
-				return durationTime*24*60
-			elif durationUnit == 'hours':
-				return durationTime*60
-			elif durationUnit == 'mins':
-				return durationTime
+			try:
+				time =  float(data[0])
+			except ValueError:
+				time = json.dumps(None)
+			
+			try:
+				unit = data[1]
+			except IndexError:
+				unit = None
+	
+			if unit == 'days':
+				return time*24*60
+			elif unit  == 'hrs' or unit == 'hr':
+				return time*60
+			elif unit  == 'mins':
+				return time
 			else:
-				return durationTime
+				return time
 
 		yield {
 			'name': extract_with_css('div.intro h2::text'),
@@ -45,7 +52,7 @@ class WalkSpider(scrapy.Spider):
 			'state': response.url.split('/')[3],
 			'location': extract_with_css('div.intro a::text'),
 			'distance': response.xpath('//div[@class="homeLists"]/div/div[@class="stat"][2]/span/text()').extract(),
-			'duration': response.xpath('//div[@class="homeLists"]/div/div[@class="stat"][3]/span/text()').extract(),
+			'duration': duration_in_min(3),
 			'difficulty': extract_with_css('.stat div::text'),
 			'features': response.css('.tagViewer .tagViewLabel::text').extract(),
 			'description': extract_with_css('.midBar p::text'),
